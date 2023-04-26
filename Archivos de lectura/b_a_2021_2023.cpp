d7e45144f71c88d1c3080456d7f78dfc783d466d
peeddroo7 <101332926+peeddroo7@users.noreply.github.com>
2023-04-26 22:43:08 +0200

    Create b_a_2021_2023.cpp

---
+++ Archivos de lectura/b_a_2021_2023.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	FILE *pf;
+	int caracter;
+	pf = fopen("C:/Info trabajo/ficheros archivo/b_a_2021_2023.txt","r");
+	if(pf==NULL)
+	{
+		printf("Error al abrir el fichero de lectura.\n");
+		return -1;
+	}
+	else
+{
+printf("Fichero abierto correctamente.\n");
+printf("\nEl contenido del archivo de prueba es \n\n");
+        while((caracter = fgetc(pf)) != EOF)
+	    {
+             printf("%c",caracter);
+	    }
+fclose(pf);
+return 0;
+}
+}
