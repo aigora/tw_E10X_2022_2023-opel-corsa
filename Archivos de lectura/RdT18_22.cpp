6a841ab4cbab397d19d6ebc0fd82d4dacd5c4583
peeddroo7 <101332926+peeddroo7@users.noreply.github.com>
2023-04-26 20:57:17 +0200

    Create RdT18_22.cpp

---
+++ Archivos de lectura/RdT18_22.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	char caracter;
+	int anyo1,anyo2;
+	FILE *pf;
+	printf("Introduce el nombre del archivo en el formato rdtYYYY\n");
+	pf = fopen("C:/Info trabajo/rdt1822/rdt1822.txt","r");
+	if(pf==NULL)
+	{
+		printf("Error al abrir el fichero de lectura.\n");
+		return -1;
+	}
+	else
+{
+printf("Fichero abierto correctamente.\n");
+printf("\nEl contenido del archivo de prueba es \n\n");
+        while((caracter = fgetc(pf)) != EOF)
+	    {
+             printf("%c",caracter);
+	    }
+fclose(pf);
+return 0;
+}
+}
