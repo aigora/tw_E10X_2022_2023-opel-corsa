7ccf8a5adbfba5f2676af56bfb9474dfe450c0b4
peeddroo7 <101332926+peeddroo7@users.noreply.github.com>
2023-05-11 16:30:40 +0200

    Update Prueba_menu_2.cpp

--- Prueba_menu_2.cpp
+++ Prueba_menu_2.cpp
@@ -1,8 +1,146 @@
 #include <stdio.h>
 
+
+void estadistica_balance()
+{
+  FILE *plectura;
+  FILE *pescritura;
+
+  float anyo19[19];
+  float anyo20[19];
+  float anyo21[19];
+  float anyo22[19];
+  float anyo23[19];
+  int i = 0;
+  
+  plectura = fopen("C:/Info trabajo/Ficheros archivo/Estadisticas_balance_2019-2023.txt", "r");
+
+  if (plectura == NULL)
+  {
+    printf("ERROR AL ABRIR EL FICHERO DE LECTURA\n");
+  }
+  else
+  {
+    for (i = 0; i <19; i++)
+    {
+      fscanf(plectura, "%f,%f,%f,%f,%f", &anyo19[i], &anyo20[i], &anyo21[i], &anyo22[i], &anyo23[i]); 
+    }
+    fclose(plectura);	
+    
+    float max_anyo19 = anyo19[0];
+    float min_anyo19 = anyo19[0];
+    float mean_anyo19 = 0;
+    float max_anyo20 = anyo20[0];
+    float min_anyo20 = anyo20[0];
+    float mean_anyo20 = 0;
+    float max_anyo21 = anyo21[0];
+    float min_anyo21 = anyo21[0];
+    float mean_anyo21 = 0;
+    float max_anyo22 = anyo22[0];
+    float min_anyo22 = anyo22[0];
+    float mean_anyo22 = 0;
+    float max_anyo23 = anyo23[0];
+    float min_anyo23 = anyo23[0];
+    float mean_anyo23 = 0;
+
+    for (i = 0; i < 19; i++)
+    {
+
+      if(anyo19[i] > max_anyo19)
+      {
+        max_anyo19 = anyo19[i];
+      }	  	
+      if(anyo19[i] < min_anyo19)
+      {
+        min_anyo19 = anyo19[i];				  		
+      }
+      mean_anyo19 = mean_anyo19 + anyo19[i];
+			
+            if(anyo20[i] > max_anyo20)
+      {
+        max_anyo20 = anyo20[i];
+      }	  	
+      if(anyo20[i] < min_anyo20)
+      {
+        min_anyo20 = anyo20[i];				  		
+      }
+      mean_anyo20 = mean_anyo20 + anyo20[i];	  	
+		  
+            if(anyo21[i] > max_anyo21)
+      {
+        max_anyo21 = anyo21[i];
+      }	  	
+      if(anyo21[i] < min_anyo21)
+      {
+        min_anyo21 = anyo21[i];				  		
+      }
+      mean_anyo21 = mean_anyo21 + anyo21[i];
+			
+            if(anyo22[i] > max_anyo22)
+      {
+        max_anyo22 = anyo22[i];
+      }	  	
+      if(anyo22[i] < min_anyo22)
+      {
+        min_anyo22 = anyo22[i];				  		
+      }
+      mean_anyo22 = mean_anyo22 + anyo22[i];
+      
+                if(anyo21[i] > max_anyo21)
+      {
+        max_anyo23 = anyo23[i];
+      }	  	
+      if(anyo23[i] < min_anyo23)
+      {
+        min_anyo23 = anyo23[i];				  		
+      }
+      mean_anyo23 = mean_anyo23 + anyo23[i];
+			
+      }
+    
+    mean_anyo19 = mean_anyo19/19;
+    mean_anyo20 = mean_anyo20/19;
+    mean_anyo21 = mean_anyo21/19;
+    mean_anyo22 = mean_anyo22/19;
+	mean_anyo23 = mean_anyo23/19;
+		
+ 
+    printf("Variables: \t\t\t Min \t\t\t Max \t\t\t Prom \n");
+    printf("Ano 2019: \t\t %f \t\t %f \t\t %f \n", min_anyo19, max_anyo19, mean_anyo19);
+    printf("Ano 2020: \t\t %f \t\t %f \t\t %f \n",min_anyo20, max_anyo20, mean_anyo20);
+    printf("Ano 2021: \t\t %f \t\t %f \t\t %f \n", min_anyo21, max_anyo21, mean_anyo21);
+    printf("Ano 2022: \t\t %f \t\t %f \t\t %f \n", min_anyo22, max_anyo22, mean_anyo22);
+    printf("Ano 2023: \t\t %f \t\t %f \t\t %f \n\n", min_anyo23, max_anyo23, mean_anyo23);
+    
+    
+    pescritura= fopen("C:/Info trabajo/Ficheros archivo/result.txt", "w");
+    if (pescritura == NULL)
+    {
+      printf("ERROR AL ABRIR EL FICHERO DE ESCRITURA");
+    }
+    else
+    {	
+      fprintf(pescritura, "Variables: \t\t Min \t\t Max \t\t Prom \n");
+      fprintf(pescritura, "Ano 2019: \t %f \t %f \t %f \n", min_anyo19, max_anyo19, mean_anyo19);
+      fprintf(pescritura, "Ano 2020: \t %f \t %f \t %f \n",min_anyo20, max_anyo20, mean_anyo20);
+      fprintf(pescritura, "Ano 2021: \t %f \t %f \t %f \n", min_anyo21, max_anyo21, mean_anyo21);
+      fprintf(pescritura, "Ano 2022: \t %f \t %f \t %f \n", min_anyo22, max_anyo22, mean_anyo22);
+      fprintf(pescritura, "Ano 2023: \t %f \t %f \t %f \n", min_anyo23, max_anyo23, mean_anyo23);
+
+      fclose(pescritura);	
+    }		
+  }
+
+}
+
+
+
+
 void subbalance1() {
     int tiempo,anyo1,anyo2,mes1,mes2;
 	printf("Estas viendo el balance energetico nacional\n");
+	printf("A continuacion, se mostraran las estadisticas mas notables sobre el balance desde el ano 2019 al 2023\n\n");
+	estadistica_balance();
 	printf("Elegir si lo quieres ver anualmente(1) o mensualmente(solo 2023)(2)\n");
 	scanf("%i",&tiempo);
 	switch(tiempo){
