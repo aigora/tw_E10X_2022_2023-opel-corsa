46bde538bd8bec030ccdfbbf3fb22a8186f4d688
peeddroo7 <101332926+peeddroo7@users.noreply.github.com>
2023-05-18 16:39:26 +0200

    Ficheros actualizados

--- Prueba_menu_2.cpp
+++ Prueba_menu_2.cpp
@@ -133,6 +133,138 @@ void estadistica_balance()
 
 }
 
+void estadistica_demanda()
+{
+  FILE *plectura;
+  FILE *pescritura;
+
+  float anyo19[2];
+  float anyo20[2];
+  float anyo21[2];
+  float anyo22[2];
+  float anyo23[2];
+  int i = 0;
+  
+  plectura = fopen("Archivos de lectura/Ficheros/Estadisticas_Demanda_2019-2023.txt", "r");
+
+  if (plectura == NULL)
+  {
+    printf("ERROR AL ABRIR EL FICHERO DE LECTURA\n");
+  }
+  else
+  {
+    for (i = 0; i <2; i++)
+    {
+      fscanf(plectura, "%f,%f,%f,%f,%f", &anyo19[i], &anyo20[i], &anyo21[i], &anyo22[i], &anyo23[i]); 
+    }
+    fclose(plectura);	
+    
+    float max_anyo19 = anyo19[0];
+    float min_anyo19 = anyo19[0];
+    float mean_anyo19 = 0;
+    float max_anyo20 = anyo20[0];
+    float min_anyo20 = anyo20[0];
+    float mean_anyo20 = 0;
+    float max_anyo21 = anyo21[0];
+    float min_anyo21 = anyo21[0];
+    float mean_anyo21 = 0;
+    float max_anyo22 = anyo22[0];
+    float min_anyo22 = anyo22[0];
+    float mean_anyo22 = 0;
+    float max_anyo23 = anyo23[0];
+    float min_anyo23 = anyo23[0];
+    float mean_anyo23 = 0;
+
+    for (i = 0; i < 2; i++)
+    {
+
+      if(anyo19[i] > max_anyo19)
+      {
+        max_anyo19 = anyo19[i];
+      }	  	
+      if(anyo19[i] < min_anyo19)
+      {
+        min_anyo19 = anyo19[i];				  		
+      }
+      mean_anyo19 = mean_anyo19 + anyo19[i];
+			
+            if(anyo20[i] > max_anyo20)
+      {
+        max_anyo20 = anyo20[i];
+      }	  	
+      if(anyo20[i] < min_anyo20)
+      {
+        min_anyo20 = anyo20[i];				  		
+      }
+      mean_anyo20 = mean_anyo20 + anyo20[i];	  	
+		  
+            if(anyo21[i] > max_anyo21)
+      {
+        max_anyo21 = anyo21[i];
+      }	  	
+      if(anyo21[i] < min_anyo21)
+      {
+        min_anyo21 = anyo21[i];				  		
+      }
+      mean_anyo21 = mean_anyo21 + anyo21[i];
+			
+            if(anyo22[i] > max_anyo22)
+      {
+        max_anyo22 = anyo22[i];
+      }	  	
+      if(anyo22[i] < min_anyo22)
+      {
+        min_anyo22 = anyo22[i];				  		
+      }
+      mean_anyo22 = mean_anyo22 + anyo22[i];
+      
+                if(anyo21[i] > max_anyo21)
+      {
+        max_anyo23 = anyo23[i];
+      }	  	
+      if(anyo23[i] < min_anyo23)
+      {
+        min_anyo23 = anyo23[i];				  		
+      }
+      mean_anyo23 = mean_anyo23 + anyo23[i];
+			
+      }
+    
+    mean_anyo19 = mean_anyo19/5;
+    mean_anyo20 = mean_anyo20/5;
+    mean_anyo21 = mean_anyo21/5;
+    mean_anyo22 = mean_anyo22/5;
+	mean_anyo23 = mean_anyo23/5;
+		
+ 
+    printf("Variables: \t\t\t Min \t\t\t Max \t\t\t Prom \n");
+    printf("Ano 2019: \t\t %f \t\t %f \t\t %f \n", min_anyo19, max_anyo19, mean_anyo19);
+    printf("Ano 2020: \t\t %f \t\t %f \t\t %f \n",min_anyo20, max_anyo20, mean_anyo20);
+    printf("Ano 2021: \t\t %f \t\t %f \t\t %f \n", min_anyo21, max_anyo21, mean_anyo21);
+    printf("Ano 2022: \t\t %f \t\t %f \t\t %f \n", min_anyo22, max_anyo22, mean_anyo22);
+    printf("Ano 2023: \t\t %f \t\t %f \t\t %f \n\n", min_anyo23, max_anyo23, mean_anyo23);
+    
+    
+    pescritura= fopen("Archivos de lectura/Ficheros/result_demanda.txt", "w");
+    if (pescritura == NULL)
+    {
+      printf("ERROR AL ABRIR EL FICHERO DE ESCRITURA");
+    }
+    else
+    {	
+      fprintf(pescritura, "Variables: \t\t Min \t\t Max \t\t Prom \n");
+      fprintf(pescritura, "Ano 2019: \t %f \t %f \t %f \n", min_anyo19, max_anyo19, mean_anyo19);
+      fprintf(pescritura, "Ano 2020: \t %f \t %f \t %f \n",min_anyo20, max_anyo20, mean_anyo20);
+      fprintf(pescritura, "Ano 2021: \t %f \t %f \t %f \n", min_anyo21, max_anyo21, mean_anyo21);
+      fprintf(pescritura, "Ano 2022: \t %f \t %f \t %f \n", min_anyo22, max_anyo22, mean_anyo22);
+      fprintf(pescritura, "Ano 2023: \t %f \t %f \t %f \n", min_anyo23, max_anyo23, mean_anyo23);
+
+      fclose(pescritura);	
+    }		
+  }
+
+}
+
 
 
 
@@ -459,8 +591,9 @@ void subdemanda1(){
 	{
 	
 				{
-					printf("Selecciona si quieres ver la demanda maxima diaria en el territorio nacional\n");
-					printf("de manera anual(1), o de manera mensual (2)\n");
+					printf("Vas a ver las estadisticas a nivel nacional\n\n");
+					estadistica_demanda();
+					printf("\nPresiona (1) para verlas de forma anual, o (2) de forma mensual");
 					scanf("%i",&tiempo);
 					if(tiempo==1)
 							{
